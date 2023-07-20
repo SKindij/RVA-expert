76dafb57b9b8c532b8c1b0dc9287ff888e3417a9
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-07-20 17:08:43 +0300

    init: set up project structure
    Create MainProduction.jsx

---
+++ src/components/main/MainProduction.jsx
@@ -0,0 +1,47 @@
+import { Grid } from '@mui/material';
+import { motion } from 'framer-motion';
+
+import {
+  TypographyHeader,
+  TypographyParagraph,
+} from '../../common/TypographyVariants';
+import MainProductionGallery from './MainProduction/MainProductionGallery';
+import MainProductionArray from './Production/MainProductionArray';
+
+const ProductionVariants = {
+  initial: { y: 150 },
+  animate: { y: 0, transition: { type: 'spring', bounce: 0.4, duration: 2 } },
+};
+
+const MotionProps = {
+  initial: 'initial',
+  whileInView: 'animate',
+  viewport: { once: true },
+  variants: ProductionVariants,
+};
+
+export default function MainProduction() {
+  return (
+    <Grid
+      sx={{ backgroundColor: '#F3F3F3' }}
+      {...MotionProps}
+      component={motion.div}
+      id="production" className="titlefix"
+    >
+      <TypographyHeader sx={{ pt: 5 }} {...MotionProps} component={motion.p}>
+        Наші об'єкти
+      </TypographyHeader>
+      <TypographyParagraph mt={5} {...MotionProps} component={motion.p}>
+        Завод оснащений усім необхідним для сучасного виробництва обладнанням. 
+		Складальний цех дозволяє здійснювати виробництво та контрольне складання систем будь-якої складності.
+      </TypographyParagraph>
+      <Grid container>
+        <MainProductionGallery
+          images={MainProductionArray}
+          MotionProps={MotionProps}
+          ProductionVariants={ProductionVariants}
+        />
+      </Grid>
+    </Grid>
+  );
+}
