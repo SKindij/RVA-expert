cf12ce313a73a7ce6655125c4da4a3cc3f654f2c
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-07-20 16:00:02 +0300

    init: set up development environment
    Create breakpoints-context.jsx

---
+++ src/components/breakpoints-context.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+// Створюємо контекст за допомогою React.createContext
+export const BreakpointsContext = React.createContext({
+  isSmall: null,
+  isMedium: null,
+  isLarge: null,
+});
+
+// Компонент провайдера, який визначає розміри екрану за допомогою MUI хуків
+export const BreakpointsContextProvider = (props) => {
+  const theme = useTheme();
+
+  // Використовуємо MUI хуки useMediaQuery для визначення розмірів екрану
+  const isSmall = useMediaQuery(theme.breakpoints.down('md'));
+  const isMedium = useMediaQuery(theme.breakpoints.between('md', 'lg'));
+  const isLarge = useMediaQuery(theme.breakpoints.up('lg'));
+
+  // Провайдер передає значення розмірів екрану через контекст дочірнім компонентам
+  return (
+    <BreakpointsContext.Provider value={{ isSmall, isMedium, isLarge }}>
+      {props.children}
+    </BreakpointsContext.Provider>
+  );
+};
