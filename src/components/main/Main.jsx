983396eba9f46ce744d13444b54721109d7119d3
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-07-20 16:23:17 +0300

    init: set up development environment
    Create Main.jsx

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,17 @@
+import MainCompany from './MainCompany';
+import MainGallery from './MainGallery';
+import MainWhyUs from './MainWhyUs';
+import MainProduction from './MainProduction';
+import MainContacts from './MainContacts';
+
+export default function Main() {
+  return (
+    <>
+      <MainCompany />
+      <MainGallery />
+      <MainWhyUs />
+      <MainProduction />
+      <MainContacts />
+    </>
+  );
+}
