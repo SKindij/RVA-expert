f7f082a331aaca3b402b6f98e56bd4db3edaf1e0
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-07-21 15:42:42 +0300

    init: set up project structure
    Create NavButtons.jsx

---
+++ src/components/navigation/NavButtons.jsx
@@ -0,0 +1,42 @@
+import { Grid, Button, Paper } from '@mui/material';
+import Link from '../common/Link';
+import { useContext } from 'react';
+import { BreakpointsContext } from '../../../../context/breakpoints-context';
+import NavSplit from './NavSplit';
+
+export const buttons = [
+  {
+    href: '/production',
+    title: 'Продукція',
+    color: 'primary',
+  },
+  {
+    href: '/calculator',
+    title: 'Розрахунок цін',
+    color: 'secondary',
+  },
+];
+
+export default function NavButtons() {
+  const ctx = useContext(BreakpointsContext);
+
+  return (
+    <>
+      {ctx.isLarge &&
+        buttons.map((button) => {
+          return (
+            <Grid item key={button.title}>
+              <Button
+                variant="contained" color={button.color}
+                component={Link} noLinkStyle
+                href={button.href} key={button.title}
+              >
+                {button.title}
+              </Button>
+            </Grid>
+          );
+        })}
+      {ctx.isMedium && <NavSplit />}
+    </>
+  );
+}
