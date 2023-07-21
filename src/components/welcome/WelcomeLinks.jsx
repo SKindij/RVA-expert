8d0179991590d1a12388da7c6a5ce02f05db443f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-07-21 11:41:31 +0300

    init: set up project structure
    Create WelcomeLinks.jsx

---
+++ src/components/welcome/WelcomeLinks.jsx
@@ -0,0 +1,57 @@
+import { Grid, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import classes from './WelcomeLinks/WelcomeLinks.module.css';
+import Link from '../common/Link';
+
+const links = [
+  {
+    title: 'Компанія',
+    href: '/#company',
+  },
+  {
+    title: 'Об\'єкти',
+    href: '/#objects',
+  },
+  {
+    title: 'Контакти',
+    href: '/#contacts',
+  },
+  {
+    title: 'Продукція',
+    href: '/production',
+  },
+];
+
+export default function () {
+  const linksVariants = {
+    initial: { x: '100vw' },
+    animate: { x: 0, transition: { duration: 2, delay: 1 } },
+  };
+
+  return (
+    <Grid
+      xs={4} sm={3}
+      item container
+      justifyContent="center" alignItems="center"
+      className={classes.welcomeLinks}
+      mt={{ xs: 8, md: 0 }}
+    >
+      {links.map((link) => {
+        return (
+          <Grid
+            item color="white"
+            component={motion.div} variants={linksVariants}
+            sx={{ width: 'fit-content' }} marginX={{ xs: 15, sm: 1.5 }}
+            key={link.title}
+          >
+            <Link href={link.href} sx={{ textDecoration: 'none' }} color="#FFF">
+              <Typography fontSize={{ xs: '1.3rem', sm: '1.5rem' }}>
+                {link.title}
+              </Typography>
+            </Link>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
