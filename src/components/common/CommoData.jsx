c9863d66a414a0ef0e20dd36f84557f0367b972f
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-07-26 16:07:04 +0300

    init: create an additional file
    components/common/CommoData.jsx

---
+++ src/components/common/CommoData.jsx
@@ -0,0 +1,33 @@
+// components/common/CommoData.jsx
+export const links = [
+    {
+      title: 'Компанія',
+      id: 'company',
+    },
+    {
+      title: 'Проекти',
+      id: 'objects',
+    },
+    {
+      title: 'Контакти',
+      id: 'contacts',
+    },
+  ];
+
+  export const buttons = [
+    {
+      href: '/rolets',
+      title: 'Ролети',
+      color: 'primary',
+    },
+    {
+      href: '/gates',
+      title: 'Ворота',
+      color: 'secondary',
+    },
+    {
+      href: '/motors',
+      title: 'Автоматика',
+      color: 'primary',
+    },
+];
