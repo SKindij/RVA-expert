03c4e004268f707a79ddfe69faa5c667d24cd4a7
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-08 20:18:15 +0300

    perf: optimize rendering performance for large datasets

--- src/components/pages/motors/VideoGallery.jsx
+++ src/components/pages/motors/VideoGallery.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Typography, Grid } from '@mui/material';
 import SectionTitleTwo from './SectionTitleTwo';
 
 const VideoGallery = ({ videoUrls }) => {
   return (
     <Box p={2} >
-	  <SectionTitleTwo title="Цікаві та пізнавальні " 
-	      colored="відеоролики по автоматиці та шлагбаумах" sx={{textAlign: 'left'}} 
-	  />
+   <SectionTitleTwo title="Цікаві та пізнавальні " 
+       colored="відеоролики по автоматиці та шлагбаумах" sx={{textAlign: 'left'}} 
+   />
       <Grid container spacing={3}>
         {videoUrls.map((video) => (
           <Grid key={video.id} item xs={12} sm={6} md={4} lg={4} xl={4}>
@@ -26,5 +26,13 @@ const VideoGallery = ({ videoUrls }) => {
     </Box>
   );
 };
-
+VideoGallery.propTypes = {
+  videoUrls: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 export default VideoGallery;
