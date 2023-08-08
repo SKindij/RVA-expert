3b6eba7c118d3d1f2ca4e770147d446df040b58a
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-08 21:56:09 +0300

    init: add necessary file

---
+++ src/components/pages/motors/MotorsCardData.js
@@ -0,0 +1,178 @@
+export const cardData = [
+    [
+	  { 
+	    id: 'A101',
+        image: 'https://img.freepik.com/free-psd/minimal-interiors-landing-page-theme_23-2148633241.jpg?w=1480&t=st=1672770221~exp=1672770821~hmac=376837f648a1fbe333e886db4087012619f88f6f3a685f43735d7f5a40ca2bff',
+        title: 'Назва продукту 1',
+        description: 'Опис і характеристики продукту 1',
+        price: '1000 UAH'
+      },
+      {
+		id: 'A102',
+        image: 'https://img.freepik.com/free-vector/mobile-app-flyer-template_52683-2978.jpg?w=826&t=st=1672770399~exp=1672770999~hmac=7648712c678c2c3be735c66700c3c52eb6a71c3942fca2363f88f2d3d19b146c',
+        title: 'Назва продукту 2',
+        description: 'Опис і характеристики продукту 2',
+        price: '2000 UAH'
+      }, 
+	  {
+		id: 'A103',
+        image: 'https://img.freepik.com/free-vector/african-american-woman-different-age_74855-6768.jpg?w=1800&t=st=1672770231~exp=1672770831~hmac=bf878ea66da2e62b602d24ce0bb3550912c4a2b8fd3beff41a855f40f7508067',
+        title: 'Назва продукту 3',
+        description: 'Опис і характеристики продукту 3',
+        price: '3000 UAH'
+      },
+	  {
+		id: 'A104',
+        image: 'https://img.freepik.com/free-vector/taxi-service-onboarding-app-screens_52683-32031.jpg?w=1380&t=st=1672770414~exp=1672771014~hmac=c42651311eec49666aad235f7ab8db20c0c3c8bd37eeb8a4f48dbf58da64b733',
+        title: 'Назва продукту 4',
+        description: 'Опис і характеристики продукту 4',
+        price: '4000 UAH'
+      },
+	  {
+		id: 'A105',
+        image: 'https://img.freepik.com/free-vector/mutual-fund-landing-page_107791-6244.jpg?w=1800&t=st=1672770241~exp=1672770841~hmac=e0accc4aae4181a02f25a74040938036b64edec55bd8b71016464c3af8fd17e7',
+        title: 'Назва продукту 5',
+        description: 'Опис і характеристики продукту 5',
+        price: '5000 UAH'
+      },
+	  {
+		id: 'A106',
+        image: 'https://img.freepik.com/free-vector/banking-transaction-app-interface_52683-41135.jpg?w=1380&t=st=1672770409~exp=1672771009~hmac=e102a83c7c073523afb2e0916fcea140c404d74d8724a2eb38f0cbf696a58660',
+        title: 'Назва продукту 6',
+        description: 'Опис і характеристики продукту 6',
+        price: '6000 UAH'
+      },
+	  {
+		id: 'A107',
+        image: 'https://img.freepik.com/free-psd/flat-design-renewable-energy-landing-page-template_23-2149476782.jpg?w=1480&t=st=1672770260~exp=1672770860~hmac=98a652a5fd2b20b136f7cc718b0bfe8a099af9cd1d4b6c6edc9b713f8b69202a',
+        title: 'Назва продукту 7',
+        description: 'Опис і характеристики продукту 7',
+        price: '7000 UAH'
+      },
+	  {
+		id: 'A108',
+        image: 'https://img.freepik.com/free-vector/banking-app-interface-concept_52683-41896.jpg?w=1380&t=st=1672770470~exp=1672771070~hmac=31f9f91155dbeb0964a7f73156d91364b97830f6ca015308866bfcda078eba84',
+        title: 'Назва продукту 8',
+        description: 'Опис і характеристики продукту 8',
+        price: '8000 UAH'
+      },
+	  {
+		id: 'A109',
+        image: 'https://img.freepik.com/free-psd/creative-writing-landing-page-template_23-2149000239.jpg?w=1480&t=st=1672770296~exp=1672770896~hmac=9b4adf12a579737f75cdb03b40caea04abf429697cc402306959bc235391659e',
+        title: 'Назва продукту 9',
+        description: 'Опис і характеристики продукту 9',
+        price: '9000 UAH'
+      },
+	  {
+		id: 'A110',
+        image: 'https://img.freepik.com/free-vector/furniture-shopping-app-concept_23-2148653684.jpg?w=1380&t=st=1672770465~exp=1672771065~hmac=1d94a51c03aa731e89ec3543c55a418acb91848d138aa8df20eab7b1474188f6',
+        title: 'Назва продукту 10',
+        description: 'Опис і характеристики продукту 10',
+        price: '10000 UAH'
+      },
+	  {
+		id: 'A111',
+        image: 'https://img.freepik.com/free-vector/eco-tourism-landing-page-with-man-illustrated_52683-44064.jpg?w=1380&t=st=1672770314~exp=1672770914~hmac=43170f54fac90bd1c020215b4a0086964e174e6e40c25df09ad3b733a38b6a45',
+        title: 'Назва продукту 11',
+        description: 'Опис і характеристики продукту 11',
+        price: '11000 UAH'
+      },	
+	  {
+		id: 'A112',
+        image: 'https://img.freepik.com/free-vector/autumn-color-tone-social-media-blog-mobile-phone-template_53876-119178.jpg?w=1380&t=st=1672770467~exp=1672771067~hmac=fafa8eb6f351b5857a7a23723b4a2791480bb68a6a1189c9920c49fd40465853',
+        title: 'Назва продукту 12',
+        description: 'Опис і характеристики продукту 12',
+        price: '12000 UAH'
+      }	  
+    ],
+    [
+       {
+		id: 'A101',
+        image: 'https://img.freepik.com/free-psd/minimal-interiors-landing-page-theme_23-2148633241.jpg?w=1480&t=st=1672770221~exp=1672770821~hmac=376837f648a1fbe333e886db4087012619f88f6f3a685f43735d7f5a40ca2bff',
+        title: 'Назва продукту 13',
+        description: 'Опис і характеристики продукту 1',
+        price: '1000 UAH'
+      },
+      {
+		id: 'A102',
+        image: 'https://img.freepik.com/free-vector/mobile-app-flyer-template_52683-2978.jpg?w=826&t=st=1672770399~exp=1672770999~hmac=7648712c678c2c3be735c66700c3c52eb6a71c3942fca2363f88f2d3d19b146c',
+        title: 'Назва продукту 2',
+        description: 'Опис і характеристики продукту 2',
+        price: '2000 UAH'
+      }, 
+	  {
+		id: 'A103',
+        image: 'https://img.freepik.com/free-vector/african-american-woman-different-age_74855-6768.jpg?w=1800&t=st=1672770231~exp=1672770831~hmac=bf878ea66da2e62b602d24ce0bb3550912c4a2b8fd3beff41a855f40f7508067',
+        title: 'Назва продукту 3',
+        description: 'Опис і характеристики продукту 3',
+        price: '3000 UAH'
+      },
+	  {
+		id: 'A104',
+        image: 'https://img.freepik.com/free-vector/taxi-service-onboarding-app-screens_52683-32031.jpg?w=1380&t=st=1672770414~exp=1672771014~hmac=c42651311eec49666aad235f7ab8db20c0c3c8bd37eeb8a4f48dbf58da64b733',
+        title: 'Назва продукту 4',
+        description: 'Опис і характеристики продукту 4',
+        price: '4000 UAH'
+      },
+    ],
+    [
+       {
+		id: 'A105',
+        image: 'https://img.freepik.com/free-vector/mutual-fund-landing-page_107791-6244.jpg?w=1800&t=st=1672770241~exp=1672770841~hmac=e0accc4aae4181a02f25a74040938036b64edec55bd8b71016464c3af8fd17e7',
+        title: 'Назва продукту 5',
+        description: 'Опис і характеристики продукту 5',
+        price: '5000 UAH'
+      },
+	  {
+		id: 'A106',
+        image: 'https://img.freepik.com/free-vector/banking-transaction-app-interface_52683-41135.jpg?w=1380&t=st=1672770409~exp=1672771009~hmac=e102a83c7c073523afb2e0916fcea140c404d74d8724a2eb38f0cbf696a58660',
+        title: 'Назва продукту 6',
+        description: 'Опис і характеристики продукту 6',
+        price: '6000 UAH'
+      },
+	  {
+		id: 'A107',
+        image: 'https://img.freepik.com/free-psd/flat-design-renewable-energy-landing-page-template_23-2149476782.jpg?w=1480&t=st=1672770260~exp=1672770860~hmac=98a652a5fd2b20b136f7cc718b0bfe8a099af9cd1d4b6c6edc9b713f8b69202a',
+        title: 'Назва продукту 7',
+        description: 'Опис і характеристики продукту 7',
+        price: '7000 UAH'
+      },
+	  {
+		id: 'A108',
+        image: 'https://img.freepik.com/free-vector/banking-app-interface-concept_52683-41896.jpg?w=1380&t=st=1672770470~exp=1672771070~hmac=31f9f91155dbeb0964a7f73156d91364b97830f6ca015308866bfcda078eba84',
+        title: 'Назва продукту 8',
+        description: 'Опис і характеристики продукту 8',
+        price: '8000 UAH'
+      },
+    ],
+	[
+	  {
+	    id: 'A109',
+        image: 'https://img.freepik.com/free-psd/creative-writing-landing-page-template_23-2149000239.jpg?w=1480&t=st=1672770296~exp=1672770896~hmac=9b4adf12a579737f75cdb03b40caea04abf429697cc402306959bc235391659e',
+        title: 'Назва продукту 9',
+        description: 'Опис і характеристики продукту 9',
+        price: '9000 UAH'
+      },
+	  {
+		id: 'A110',
+        image: 'https://img.freepik.com/free-vector/furniture-shopping-app-concept_23-2148653684.jpg?w=1380&t=st=1672770465~exp=1672771065~hmac=1d94a51c03aa731e89ec3543c55a418acb91848d138aa8df20eab7b1474188f6',
+        title: 'Назва продукту 10',
+        description: 'Опис і характеристики продукту 10',
+        price: '10000 UAH'
+      },
+	  {
+		id: 'A111',
+        image: 'https://img.freepik.com/free-vector/eco-tourism-landing-page-with-man-illustrated_52683-44064.jpg?w=1380&t=st=1672770314~exp=1672770914~hmac=43170f54fac90bd1c020215b4a0086964e174e6e40c25df09ad3b733a38b6a45',
+        title: 'Назва продукту 11',
+        description: 'Опис і характеристики продукту 11',
+        price: '11000 UAH'
+      },	
+	  {
+		id: 'A112',
+        image: 'https://img.freepik.com/free-vector/autumn-color-tone-social-media-blog-mobile-phone-template_53876-119178.jpg?w=1380&t=st=1672770467~exp=1672771067~hmac=fafa8eb6f351b5857a7a23723b4a2791480bb68a6a1189c9920c49fd40465853',
+        title: 'Назва продукту 12',
+        description: 'Опис і характеристики продукту 12',
+        price: '12000 UAH'
+      }	  
+	]
+];
