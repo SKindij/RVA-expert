575aa9134fe6a2212d9a7c59e9560dd0a916638a
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-08 20:48:16 +0300

    chore: make general project maintenance changes

--- src/components/common/Copyright.jsx
+++ src/components/common/Copyright.jsx
@@ -2,13 +2,13 @@ import Typography from '@mui/material/Typography';
 
 export default function Copyright() {
   return (
-    <>
+    <section style={{ marginTop: '20px' }}>
       <Typography variant="body2" color="text.secondary" align="center">
         {'Copyright © Tech Solutions'}{' '}{new Date().getFullYear()}
       </Typography>
       <Typography variant="body2" color="text.secondary" align="center">
-        {'rva.expert v.0.3.5 by SKindij'}
+        {'rva.expert v.0.4.1 by SKindij'}
       </Typography>
-    </>
+    </section>
   );
 }
