14562642068292c350b5c2b258f03e1ef7f0b740
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-25 21:12:11 +0300

    refactor: extract a reusable component from the existing code

--- src/components/common/PageGoods.jsx
+++ src/components/common/PageGoods.jsx
@@ -1,66 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Grid } from '@mui/material';
 import SectionTitle from './SectionTitle';
 import GoodsGallery from './GoodsGallery';
 import GoodsContent from './GoodsContent';
 
-const GatesGoods = () => {
- 
+const PageGoods = ({ blocksData }) => {
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%' }}>
       <SectionTitle title="Разом із нами" colored="можливо все!" />
       <Grid container spacing={4} sx={{ flexGrow: 1, width: '100%', padding: '1rem' }}>
-
-        {/* Перший блок */}
-        <Grid item xs={12} md={7}>
-          <GoodsGallery images={blockAData.images} alts={blockAData.alts} />
-        </Grid>
-        <Grid item xs={12} md={5}>
-          <GoodsContent
-            title={blockAData.title}
-            firstDescription={blockAData.firstDescription}
-            secondDescription={blockAData.secondDescription}
-          />
-        </Grid>   
-
-        {/* Другий блок */}
-        <Grid item xs={12} md={5}>
-          <GoodsContent
-            title={blockBData.title}
-            firstDescription={blockBData.firstDescription}
-            secondDescription={blockBData.secondDescription}
-          />
-        </Grid> 
-        <Grid item xs={12} md={7}>
-          <GoodsGallery images={blockBData.images} alts={blockBData.alts}/>
-        </Grid>
-
-        {/* Третій блок */}
-        <Grid item xs={12} md={7}>
-          <GoodsGallery images={blockCData.images} alts={blockCData.alts}/>
-        </Grid>
-        <Grid item xs={12} md={5}>
-          <GoodsContent
-            title={blockCData.title}
-            firstDescription={blockCData.firstDescription}
-            secondDescription={blockCData.secondDescription}
-          />
-        </Grid> 
-
-        {/* Четвертий блок */}
-        <Grid item xs={12} md={5}>
-          <GoodsContent
-            title={blockDData.title}
-            firstDescription={blockDData.firstDescription}
-            secondDescription={blockDData.secondDescription}
-          />
-        </Grid>
-        <Grid item xs={12} md={7}>
-          <GoodsGallery images={blockDData.images} alts={blockDData.alts}/>
-        </Grid>
-
-      </Grid>  
+  {blocksData.map((blockData, index) => (
+    <React.Fragment key={index}>
+      {index % 2 === 0 ? (
+        // Парна ітерація: <GoodsContent/> ліворуч, <GoodsGallery/> праворуч
+        <>
+          <Grid item xs={12} md={7}>
+            <GoodsContent
+              title={blockData.title}
+              firstDescription={blockData.firstDescription}
+              secondDescription={blockData.secondDescription}
+            />
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <GoodsGallery images={blockData.images} alts={blockData.alts} />
+          </Grid>
+        </>
+      ) : (
+        // Непарна ітерація: <GoodsGallery/> ліворуч, <GoodsContent/> праворуч
+        <>
+          <Grid item xs={12} md={5}>
+            <GoodsGallery images={blockData.images} alts={blockData.alts} />
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <GoodsContent
+              title={blockData.title}
+              firstDescription={blockData.firstDescription}
+              secondDescription={blockData.secondDescription}
+            />
+          </Grid>
+        </>
+      )}
+    </React.Fragment>
+  ))}
+</Grid>
     </Box>
   );
 };
+PageGoods.propTypes = {
+  blocksData: PropTypes.array.isRequired, // Замініть на відповідний тип даних
+};
 
-export default GatesGoods;
+export default PageGoods;
